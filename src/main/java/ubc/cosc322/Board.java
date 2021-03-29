f64cf7b57bb7ea7057f8570c5ce95626944ff327
Mack <mackschmaltz@gmail.com>
2021-03-29 00:27:13 -0600

    changed heuristic to only need the board to calculate value

--- src/main/java/ubc/cosc322/Board.java
+++ src/main/java/ubc/cosc322/Board.java
@@ -81,6 +81,16 @@ public class Board {
         return pos;
     }
 
+    public ArrayList<Coordinate> getArrowCoordinates(){
+        ArrayList<Coordinate> pos = new ArrayList();
+        for(int r=0; r< board.length; ++r)
+            for(int c=0; c<board[r].length; ++c)
+                if(board[r][c] == 3){
+                    pos.add(new Coordinate(r, c));
+                }
+        return pos;
+    }
+
     public ArrayList<Coordinate> getReachableCoordinates(Coordinate queenCurr){
         ArrayList<Coordinate> moves = new ArrayList<>();
         //Straights
@@ -196,6 +206,7 @@ public class Board {
         return moves;
     }
 
+    // Deprecated test function
     public int countPossibleMoves(int queenNum){
         int count = 0;
         for(Coordinate queenCurr: getQueenCoordinates(queenNum))
@@ -214,304 +225,85 @@ public class Board {
 
     // NOTE: Consider moving heuristic calculations to its own class
 
-    public double getHeuristic(Move moveMade, int playerQueenNum){
-        Coordinate currentPos = moveMade.getQueenCurr();
-        Coordinate newPos = moveMade.getQueenMove();
-        Coordinate arrowPos = moveMade.getArrow();
-        // Total value of the move
+    public double getHeuristic(){
+        // Total value of the board
         // Positive is good for player 1, negative is good for player 2
         // The reasoning for the positive/negative decision is described in GTNode.java
         double moveValue = 0;
 
-        // See if moving closer or further from placed arrows
-        if(playerQueenNum == 1){
-            // Arrow distances are particularly useful early on, less so in the late game
-            moveValue += getNearestArrowDistance(newPos)-getNearestArrowDistance(currentPos);
-            moveValue += checkSurroundings(newPos, playerQueenNum) - checkSurroundings(currentPos, playerQueenNum);
-            moveValue += arrowAiming(arrowPos, playerQueenNum);
-            moveValue += getNumberOfMoves(newPos);
+        for (int team = 1; team<3;team++){
+            // valueCounter checks the value of the board for each team individually
+            // then adds it the total value
+            int valueCounter = 0;
+            valueCounter += getNumberOfMoves(team);
+            valueCounter += arrowAimingIntermediary(team);
+            // Change the sign for the value of team 2 moves
+            if (team == 2)
+                valueCounter = valueCounter*-1;
+            moveValue += valueCounter;
         }
-        else {
-            moveValue -= getNearestArrowDistance(newPos)-getNearestArrowDistance(currentPos);
-            moveValue -= checkSurroundings(newPos, playerQueenNum) - checkSurroundings(currentPos, playerQueenNum);
-            moveValue -= arrowAiming(arrowPos, playerQueenNum);
-            moveValue -= getNumberOfMoves(newPos);
-        }
-
 
         return moveValue;
     }
 
-    // Determine the number of possible moves a queen will have in a space
-    private double getNumberOfMoves(Coordinate queenPos){
+    // Determine the number of possible moves a team will have
+    private double getNumberOfMoves(int queenNum){
         int numMoves = 0;
-        for (Coordinate queenMove : getReachableCoordinates(queenPos)) {
-            for (Coordinate arrow : getReachableCoordinates(queenMove))
-                // Ensure that the number of moves is weighted heavily
-                numMoves += 3;
-        }
-
+        for(Coordinate queenCurr: getQueenCoordinates(queenNum))
+            for (Coordinate queenMove : getReachableCoordinates(queenCurr)) {
+                for (Coordinate arrow : getReachableCoordinates(queenMove))
+                    // Ensure the number of moves is heavily weighted
+                    numMoves+=3;
+            }
         return numMoves;
     }
 
-    // Method to turn arrow coordinates back into the absolute distance
-    private double getNearestArrowDistance(Coordinate queenPos){
-        Coordinate arrowPos = getNearestArrowPosition(queenPos);
-        int xdif = Math.abs(arrowPos.getX()-queenPos.getX());
-        int ydif = Math.abs(arrowPos.getY()-queenPos.getY());
-        if (xdif == 0 && ydif == 0)
-            return 0;
-        return Math.sqrt((xdif*xdif)+(ydif*ydif));
-    }
-
-    // Method for determining Coordinates of arrow
-    private Coordinate getNearestArrowPosition(Coordinate queenPos){
-        double distanceToArrow = 100;
-        Coordinate arrowPos = queenPos;
-
-        // Run through all Y rows
-        for (int y = 0; y<10; y++){
-            // Run through all X columns
-            for (int x = 0; x<10; x++){
-                if (board[y][x] == 3){
-                    int xdif = Math.abs(x-queenPos.getX());
-                    int ydif = Math.abs(y-queenPos.getY());
-                    // Determine absolute distance
-                    double newDistance = Math.sqrt((xdif*xdif)+(ydif*ydif));
-                    if (newDistance < distanceToArrow) {
-                        distanceToArrow = newDistance;
-                        arrowPos = new Coordinate(y,x);
-                    }
-                }
-            }
-        }
-        return arrowPos;
+    // Determine the number of possible moves a team will have
+    private double arrowAimingIntermediary(int queenNum){
+        int arrowValues = 0;
+        for (Coordinate arrow : getArrowCoordinates())
+            arrowValues += arrowAiming(arrow, queenNum);
+        return arrowValues;
     }
 
     // Method for determining value of an arrow placement
+    // Previously checked what team the nearest queen was,
+    // will now check what queen an arrow is near
     private double arrowAiming(Coordinate arrowPos, int playerQueenNum){
-        double distanceToQueen = 100;
-        int queenTeam = playerQueenNum;
+        int arrowValue = 0;
 
         // Run through all Y rows
         for (int y = 0; y<10; y++){
             // Run through all X columns
             for (int x = 0; x<10; x++){
                 // Check if any queen is on the space, the computer will then determine if that's good or bad
-                if (board[y][x] == 1 || board[y][x] == 2){
+                if ((board[y][x] == 1 || board[y][x] == 2) &&
+                        ((x == arrowPos.getX()-1 || x == arrowPos.getX()+1 || x == arrowPos.getX()) &&
+                        (y == arrowPos.getY()-1 || y == arrowPos.getY()+1 || y == arrowPos.getY()))){
+                    // Ignore space the arrow is on
+                    if(x == arrowPos.getX() && y == arrowPos.getY())
+                        continue;
+
                     // Determine which team is on the space
                     int queenCheck = board[y][x];
-                    int xdif = Math.abs(x-arrowPos.getX());
-                    int ydif = Math.abs(y-arrowPos.getY());
-                    // Determine absolute distance
-                    double aimDistance = Math.sqrt((xdif*xdif)+(ydif*ydif));
-                    // If two queens are the same distance from the arrow, focus on the queen on the player's team
-                    if (aimDistance <= distanceToQueen && queenCheck == playerQueenNum) {
-                        queenTeam = playerQueenNum;
-                        distanceToQueen = aimDistance;
+
+                    // Heavily penalize arrows next to a friendly queen
+                    if (queenCheck == playerQueenNum) {
+                        arrowValue -= 2;
                     }
-                    else if (aimDistance < distanceToQueen && queenCheck != playerQueenNum){
-                        queenTeam = queenCheck;
-                        distanceToQueen = aimDistance;
+
+                    // Support arrows next to enemy
+                    else if (queenCheck != 0 && queenCheck != 3){
+                        arrowValue += 1;
                     }
                 }
             }
         }
-
-        // The move is very poor if the arrow is placed close to a friendly queen
-        if (queenTeam == playerQueenNum){
-            distanceToQueen = distanceToQueen * -0.5;
-        }
-
-        // Simple equation so that the value of placing an arrow closer to a queen is greater magnitude
-        return 10/distanceToQueen;
-    }
-
-    // Method for determining how surrounded the queen is or will be
-    private int checkSurroundings(Coordinate queenPos, int playerQueenNum){
-        int totalValue = 0;
-        int xPos = queenPos.getX();
-        int yPos = queenPos.getY();
-
-        // If not along an edge
-        if(yPos < 9 && yPos > 0 && xPos < 9 && xPos >0){
-            for(int a = 0; a < 3; a++){
-                // Don't want to move all queens into the same spot
-                if (board[yPos+1][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos+1][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                // Space is clear
-                else
-                    totalValue +=1;
-            }
-            for (int a = 0; a < 3; a++){
-                // Ignore current position
-                if (a == 1)
-                    continue;
-
-                // Don't want to move all queens into the same spot
-                if (board[yPos][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-            for (int a = 0; a < 3; a++){
-                // Don't want to move all queens into the same spot
-                if(board[yPos-1][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos-1][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-        }
-        // If along the upper edge
-        else if (yPos == 9 && xPos < 9 && xPos >0){
-            // Account for missing spaces
-            totalValue -= 3;
-            for (int a = 0; a < 3; a++){
-                // Ignore current position
-                if (a == 1)
-                    continue;
-
-                // Don't want to move all queens into the same spot
-                if (board[yPos][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-            for (int a = 0; a < 3; a++){
-                // Don't want to move all queens into the same spot
-                if(board[yPos-1][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos-1][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-        }
-        // If along the lower edge
-        else if (yPos == 0 && xPos < 9 && xPos >0){
-            // Account for missing spaces
-            totalValue -= 3;
-            for (int a = 0; a < 3; a++){
-                // Ignore current position
-                if (a == 1)
-                    continue;
-
-                // Don't want to move all queens into the same spot
-                if (board[yPos][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-            for (int a = 0; a < 3; a++){
-                // Don't want to move all queens into the same spot
-                if(board[yPos+1][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos+1][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-        }
-        // If along the right edge
-        else if(yPos < 9 && yPos > 0 && xPos == 9){
-            totalValue -= 3;
-            for(int a = 0; a < 2; a++){
-                // Don't want to move all queens into the same spot
-                if (board[yPos+1][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos+1][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-            for (int a = 0; a < 2; a++){
-                // Ignore current position
-                if (a == 1)
-                    continue;
-
-                // Don't want to move all queens into the same spot
-                if (board[yPos][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-            for (int a = 0; a < 2; a++){
-                // Don't want to move all queens into the same spot
-                if(board[yPos-1][(xPos-1)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos-1][(xPos-1)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-        }
-        // If along the left edge
-        else if(yPos < 9 && yPos > 0 && xPos == 0){
-            totalValue -= 3;
-            for(int a = 0; a < 2; a++){
-                // Don't want to move all queens into the same spot
-                if (board[yPos+1][(xPos)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos+1][(xPos)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-            for (int a = 0; a < 2; a++){
-                // Ignore current position
-                if (a == 0)
-                    continue;
-
-                // Don't want to move all queens into the same spot
-                if (board[yPos][(xPos)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos][(xPos)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-            for (int a = 0; a < 2; a++){
-                // Don't want to move all queens into the same spot
-                if(board[yPos-1][(xPos)+a] == playerQueenNum)
-                    totalValue -= 2;
-                else if (board[yPos-1][(xPos)+a] != 0)
-                    totalValue -=1;
-                    // Space is clear
-                else
-                    totalValue +=1;
-            }
-        }
-        // Avoid corners, they restrict movement significantly
-        else
-            totalValue -= 7;
-
-        return totalValue;
+        return arrowValue;
     }
 
 
+    // Deprecated test function
     public double getRatioOurMovesToOpponentMoves(){
         double blackMoves = countPossibleMoves(2);
         double whiteMoves = countPossibleMoves(1);
